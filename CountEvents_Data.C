59fcf4cd2a489d39f66b7a2a64bcfbdab122662c
Yuichi <yuichiok@buffalo.edu>
2017-05-11 02:42:58 -0400

    added bool for HFtag

--- CountEvents_Data.C
+++ CountEvents_Data.C
@@ -32,11 +32,11 @@ void CountEvents_Data(){
     float m_Z_mass, m_Z_pt, m_Z_eta, m_Z_phi;
     float m_jet_pt[maxNumJets], m_jet_eta[maxNumJets], m_jet_phi[maxNumJets], m_jet_msv[maxNumJets];
     float m_jet_csv[maxNumJets];
-	float m_jet_vtxCat_IVF[maxNumJets];
-	float m_jet_msv_new[maxNumJets];
-	float m_jet_msv_inc[maxNumJets];
-	float m_jet_vtxMassCorr_IVF[maxNumJets];
-
+    float m_jet_vtxCat_IVF[maxNumJets];
+    float m_jet_msv_new[maxNumJets];
+    float m_jet_msv_inc[maxNumJets];
+    float m_jet_vtxMassCorr_IVF[maxNumJets];
+    
     float m_MET_et;
     float m_MET_phi;
     float m_MET_sumet;
@@ -46,19 +46,19 @@ void CountEvents_Data(){
     unsigned int m_run;
     unsigned int m_lumi;
     
-	std::map<TString, std::map<TString, std::vector<bool> > > HFJets   ;
-	std::map<TString, std::map<TString,              bool > > hasHFJets;
-	std::map<TString, std::map<TString,               int > > leadHFJet;
-
-	const vector<TString> EventHandler::HFTags = {"NoHF", "CSVL", "CSVM", "CSVT", "CSVS"};
-	const vector<TString> EventHandler::SVType = {"noSV", "pfSV", "pfISV", "cISV", "cISVf", "cISVp"};
-
+    std::map<TString, std::map<TString, std::vector<bool> > > HFJets   ;
+    std::map<TString, std::map<TString,              bool > > hasHFJets;
+    std::map<TString, std::map<TString,               int > > leadHFJet;
+    
+    const vector<TString> EventHandler::HFTags = {"NoHF", "CSVL", "CSVM", "CSVT", "CSVS"};
+    const vector<TString> EventHandler::SVType = {"noSV", "pfSV", "pfISV", "cISV", "cISVf", "cISVp"};
+    
     std::map<TString, float*> SVVariable;
     std::map<TString, float > SVMinimumVal;
     std::map<TString, float*> HFTagDiscrimVar;
-	std::map<TString, float > HFTagDiscrimOP ;
-
-
+    std::map<TString, float > HFTagDiscrimOP ;
+    
+    
     TTreeReader myReader("EventTree", f);
     TTree *tree = (TTree*)f->Get("tree");
     
@@ -82,13 +82,13 @@ void CountEvents_Data(){
     tree->SetBranchAddress( "Jet_phi"        ,  m_jet_phi     );
     tree->SetBranchAddress( "Vtype"          , &m_Vtype       );
     tree->SetBranchAddress( "Jet_vtxMass"    ,  m_jet_msv     );
-
-	tree->SetBranchAddress( "Jet_vtxCat_IVF"     , m_jet_vtxCat_IVF     );
-	tree->SetBranchAddress( "Jet_vtxMassCorr_IVF", m_jet_vtxMassCorr_IVF);
-	tree->SetBranchAddress( "Jet_btagCSV"    ,  m_jet_csv     );
-	tree->SetBranchAddress( "Jet_newVtxMass"     , m_jet_msv_new        );
-	tree->SetBranchAddress( "Jet_incVtxMass"     , m_jet_msv_inc        );
-
+    
+    tree->SetBranchAddress( "Jet_vtxCat_IVF"     , m_jet_vtxCat_IVF     );
+    tree->SetBranchAddress( "Jet_vtxMassCorr_IVF", m_jet_vtxMassCorr_IVF);
+    tree->SetBranchAddress( "Jet_btagCSV"    ,  m_jet_csv     );
+    tree->SetBranchAddress( "Jet_newVtxMass"     , m_jet_msv_new        );
+    tree->SetBranchAddress( "Jet_incVtxMass"     , m_jet_msv_inc        );
+    
     tree->SetBranchAddress( "met_pt"     , &m_MET_et    );
     tree->SetBranchAddress( "met_phi"    , &m_MET_phi   );
     tree->SetBranchAddress( "met_sumEt"  , &m_MET_sumet );
@@ -98,7 +98,34 @@ void CountEvents_Data(){
     tree->SetBranchAddress( "run" , &m_run  );
     tree->SetBranchAddress( "lumi", &m_lumi );
     
+    HFTagDiscrimVar["NoHF"] = m_jet_csv;
+    HFTagDiscrimVar["CSVL"] = m_jet_csv;
+    HFTagDiscrimVar["CSVM"] = m_jet_csv;
+    HFTagDiscrimVar["CSVT"] = m_jet_csv;
+    HFTagDiscrimVar["CSVS"] = m_jet_csv;
+    HFTagDiscrimOP ["NoHF"] = 1.00;
+    HFTagDiscrimOP ["CSVL"] = 0.98;
+    HFTagDiscrimOP ["CSVM"] = 0.95;
+    HFTagDiscrimOP ["CSVT"] = 0.90;
+    HFTagDiscrimOP ["CSVS"] = 0.90;
     
+    SVVariable["noSV" ] = m_jet_msv             ;
+    //SVVariable["oldSV"] = m_jet_msv             ;
+    SVVariable["pfSV" ] = m_jet_msv_new         ;
+    SVVariable["pfISV"] = m_jet_msv_inc         ;
+    //SVVariable["qcSV" ] = jet_msv_quickCorr     ;
+    SVVariable["cISV" ] = m_jet_vtxMassCorr_IVF ;
+    SVVariable["cISVf"] = m_jet_vtxMassCorr_IVF ;
+    SVVariable["cISVp"] = m_jet_vtxMassCorr_IVF ;
+    SVMinimumVal["noSV" ] = -100.0 ;
+    //SVMinimumVal["oldSV"] = anCfg.minSVT ;
+    SVMinimumVal["pfSV" ] = 0.0 ;
+    SVMinimumVal["pfISV"] = 0.0 ;
+    //SVMinimumVal["qcSV" ] = anCfg.minSVT ;
+    SVMinimumVal["cISV" ] = 0.0 ;
+    SVMinimumVal["cISVf"] = 0.0 ;
+    SVMinimumVal["cISVp"] = 0.0 ;
+
     
     TCanvas *c1 = new TCanvas("c1","multipad1",1000,600);
     TCanvas *c2 = new TCanvas("c2","multipad2",1000,600);
@@ -130,13 +157,13 @@ void CountEvents_Data(){
     TH1F *MuonEtaBf2        = new TH1F("muonEtabf2","Muon #eta (w/o cut)",100,-4,4);
     TH1F *MuonPt2           = new TH1F("muonPt2","Muon p_T (Z+jet w/MET cut)",100,0,300.);
     TH1F *MuonEta2          = new TH1F("muonEta2","Muon #eta (Z+jet w/MET cut)",100,-4,4);
-	TH1F *JetMult           = new TH1F("jetMult","Jet Multiplicity (Valid Z-boson)",10,0,10.);
+    TH1F *JetMult           = new TH1F("jetMult","Jet Multiplicity (Valid Z-boson)",10,0,10.);
     TH1F *JetPt             = new TH1F("jetPt","Jet p_T (Z+jet w/MET cut)",100,0,200.);
     TH1F *JetEta            = new TH1F("jetEta","Jet #eta (Z+jet w/MET cut)",100,-4,4);
     TH1F *nMET				= new TH1F("nmet","MET (Valid Z-boson)",100,0,200.);
-	TH1F *MSV				= new TH1F("msv","M_{SV} (Z+jet w/MET cut)",100,0,20.);
+    TH1F *MSV				= new TH1F("msv","M_{SV} (Z+jet w/MET cut)",100,0,20.);
     
-	TH1F *DY_Pt             = new TH1F("DYPt","pt distribution of DY process",100,0,300.);
+    TH1F *DY_Pt             = new TH1F("DYPt","pt distribution of DY process",100,0,300.);
     TH1F *DY_Eta            = new TH1F("DYEta","#eta distribution of DY process",100,-4,4);
     
     //setting color scheme
@@ -145,39 +172,39 @@ void CountEvents_Data(){
     MuonPtBf2->SetFillColorAlpha(kBlue,0.35);
     MuonPtBf2->SetFillStyle(3001);
     
-	MuonEtaBf1->SetFillColorAlpha(kRed,0.35);
+    MuonEtaBf1->SetFillColorAlpha(kRed,0.35);
     MuonEtaBf1->SetFillStyle(3002);
     MuonEtaBf2->SetFillColorAlpha(kBlue,0.35);
     MuonEtaBf2->SetFillStyle(3001);
     
-	MuonPt1->SetFillColorAlpha(kRed,0.35);
+    MuonPt1->SetFillColorAlpha(kRed,0.35);
     MuonPt1->SetFillStyle(3002);
     MuonPt2->SetFillColorAlpha(kBlue,0.35);
     MuonPt2->SetFillStyle(3001);
     
-	MuonEta1->SetFillColorAlpha(kRed,0.35);
+    MuonEta1->SetFillColorAlpha(kRed,0.35);
     MuonEta1->SetFillStyle(3002);
     MuonEta2->SetFillColorAlpha(kBlue,0.35);
     MuonEta2->SetFillStyle(3001);
     
-	zbosonBf->SetFillColorAlpha(kRed,0.35);
+    zbosonBf->SetFillColorAlpha(kRed,0.35);
     zbosonBf->SetFillStyle(3002);
     zboson->SetFillColorAlpha(kBlue,0.35);
     zboson->SetFillStyle(3001);
-
-	JetMult->SetFillColorAlpha(kRed,0.35);
-	JetMult->SetFillStyle(3001);
-	JetPt->SetFillColorAlpha(kRed,0.35);
-	JetPt->SetFillStyle(3001);
-	JetEta->SetFillColorAlpha(kRed,0.35);
-	JetEta->SetFillStyle(3001);
-	nPV->SetFillColorAlpha(kRed,0.35);
-	nPV->SetFillStyle(3001);
-	nMET->SetFillColorAlpha(kRed,0.35);
-	nMET->SetFillStyle(3001);
-	MSV->SetFillColorAlpha(kRed,0.35);
-	MSV->SetFillStyle(3001);
-
+    
+    JetMult->SetFillColorAlpha(kRed,0.35);
+    JetMult->SetFillStyle(3001);
+    JetPt->SetFillColorAlpha(kRed,0.35);
+    JetPt->SetFillStyle(3001);
+    JetEta->SetFillColorAlpha(kRed,0.35);
+    JetEta->SetFillStyle(3001);
+    nPV->SetFillColorAlpha(kRed,0.35);
+    nPV->SetFillStyle(3001);
+    nMET->SetFillColorAlpha(kRed,0.35);
+    nMET->SetFillStyle(3001);
+    MSV->SetFillColorAlpha(kRed,0.35);
+    MSV->SetFillStyle(3001);
+    
     
     std::cout << "Total Events: " << nEntry << std::endl;
     
@@ -220,11 +247,11 @@ void CountEvents_Data(){
             if( m_lep_pt[1] > 20 && abs(m_lep_eta[1]) < 2.4 && m_lep_iso[1] < 0.25) Lep1_Check = true;
             
             if( Zmass_Check==true ){
-
-				zboson->Fill(m_Z_mass);
+                
+                zboson->Fill(m_Z_mass);
                 nMET->Fill(m_MET_et);
                 JetMult->Fill(m_nJets);
-
+                
                 if(Lep0_Check==true && Lep1_Check==true && MET_Check==true){
                     
                     nPV->Fill(m_nPVs);
@@ -241,9 +268,9 @@ void CountEvents_Data(){
                         MuonEta2->Fill(m_lep_eta[0]);
                     }
                     
-					//Jet analysis
-					vector<int> validJets;
-					validJets.clear();
+                    //Jet analysis
+                    vector<int> validJets;
+                    validJets.clear();
                     for(int j=0; j<m_nJets; j++){
                         
                         if( m_jet_pt[j] < 30. || abs(m_jet_eta[j]) > 2.4) continue;
@@ -251,30 +278,43 @@ void CountEvents_Data(){
                         JetPt->Fill(m_jet_pt[j]);
                         JetEta->Fill(m_jet_eta[j]);
                         MSV->Fill(m_jet_msv[j]);
-
-						int lowPtIndex = j;
-						for(int k=0; k<validJets.size(); k++)
-						{
-							if(m_jet_pt[validJets[k]]<m_jet_pt[lowPtIndex]) swap(validJets[k], lowPtIndex);
-						}
-						validJets.push_back(lowPtIndex);
-
+                        
+                        int lowPtIndex = j;
+                        for(int k=0; k<validJets.size(); k++)
+                        {
+                            if(m_jet_pt[validJets[k]]<m_jet_pt[lowPtIndex]) swap(validJets[k], lowPtIndex);
+                        }
+                        validJets.push_back(lowPtIndex);
+                        
                     }
-
-					for(auto& svType : SVType)
-						for(auto& hfTag : HFTags)
-							{	HFJets   [hfTag][svType] = vector<bool>(validJets.size(), false);
-								hasHFJets[hfTag][svType] = false;
-							}
-
-					for(int vJet_i=0, evt_i=0; vJet_i<validJets.size(); vJet_i++){
-						
-						evt_i = validJets[vJet_i];
-						
-
-
-
-					}
+                    
+                    for(auto& svType : SVType)
+                        for(auto& hfTag : HFTags)
+                        {	HFJets   [hfTag][svType] = vector<bool>(validJets.size(), false);
+                            hasHFJets[hfTag][svType] = false;
+                            leadHFJet[hfTag][svType] = -1;
+                        }
+                    
+                    for(int vJet_i=0, evt_i=0; vJet_i<validJets.size(); vJet_i++){
+                        
+                        evt_i = validJets[vJet_i];
+                        for(auto& svType : SVType)
+                            for(auto& hfTag : HFTags)
+                                if(HFTagDiscrimVar[hfTag][evt_i] >= HFTagDiscrimOP[hfTag] && SVVariable[svType][evt_i] >= SVMinimumVal[svType])
+                                { // Unfortunate hardcoded checking: check the vertex category of the corrected secondary vertices, if the svType is appropriate.
+                                    // cISVf = full SV reco'd (==0)
+                                    // cISVp = psuedo vertex (==1)
+                                    if(svType == "cISVf" && m_jet_vtxCat_IVF[evt_i] != 0) continue;
+                                    if(svType == "cISVp" && m_jet_vtxCat_IVF[evt_i] != 1) continue;
+                                    // Set HFSVtag Variables.
+                                    HFJets[hfTag][svType][vJet_i] = true;
+                                    if(!hasHFJets[hfTag][svType])
+                                    {   leadHFJet[hfTag][svType] = vJet_i;
+                                        hasHFJets[hfTag][svType] = true;
+                                    }
+                                }
+                        
+                    }
                     
                 }
             }
@@ -306,31 +346,31 @@ void CountEvents_Data(){
     gPad->SetLogy();
     zboson->Draw("");
     
-	c3->cd(1);
-	gPad->SetLogy();
-	nMET->Draw("");
-	c3->cd(2);
-	gPad->SetLogy();
-	JetMult->Draw("");
-	c3->cd(3);
-	gPad->SetLogy();
-	MSV->Draw("");
-	c3->cd(4);
-	gPad->SetLogy();
-	nPV->Draw("");
-	c3->cd(5);
-	gPad->SetLogy();
-	JetPt->Draw("");
-	c3->cd(6);
-	gPad->SetLogy();
-	JetEta->Draw("");
-
-	c1->Print("Lepton.pdf");
-	c2->Print("Z-boson.pdf");
-	c3->Print("Jet.pdf");
-
-	gSystem->Exit(0);
-
-
+    c3->cd(1);
+    gPad->SetLogy();
+    nMET->Draw("");
+    c3->cd(2);
+    gPad->SetLogy();
+    JetMult->Draw("");
+    c3->cd(3);
+    gPad->SetLogy();
+    MSV->Draw("");
+    c3->cd(4);
+    gPad->SetLogy();
+    nPV->Draw("");
+    c3->cd(5);
+    gPad->SetLogy();
+    JetPt->Draw("");
+    c3->cd(6);
+    gPad->SetLogy();
+    JetEta->Draw("");
+    
+    c1->Print("Lepton.pdf");
+    c2->Print("Z-boson.pdf");
+    c3->Print("Jet.pdf");
+    
+    gSystem->Exit(0);
+    
+    
     
 }
